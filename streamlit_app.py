bc49311231b3092e8a456872054bae8050762cf4
Kinley Wangchuk <94952098+KinWang-2013@users.noreply.github.com>
2024-10-12 08:37:46 +0000

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,14 +1,13 @@
 import streamlit as st
 from datetime import datetime
 
-# Set the page config at the very beginning
-st.set_page_config(page_title="Bhutan Tourism Income Predictor", page_icon="🇧🇹", layout="wide")
+st.set_page_config(page_title="Bhutan Tourism Predictor", page_icon="🇧🇹", layout="wide")
 
 st.markdown(
     """
     <style>
     .stApp {
-        background-image: url("https://b-cdn.springnest.com/media/img/kd/4034690728_bc032ab670_o5087a9c.jpg?width=800");
+        background-image: url();
         background-size: cover;
         opacity: 0.7;
     }
@@ -31,10 +30,10 @@ st.markdown(
     unsafe_allow_html=True
 )
 
-st.title("Bhutan Tourism Income Predictor")
-st.write("Predict the tourism income for Bhutan based on the year input.")
+st.title("Bhutan Tourism Predictor")
+st.write("Predict the number of tourists for Bhutan based on the year input.")
 
 year = st.number_input("Enter the year:", min_value=2024, max_value=2100, value=2024, step=1)
 
 predicted_income = 200
-st.write(f"Predicted tourism income for the year {year}: Nu. {predicted_income:,.2f}")
+st.write(f"Predicted number of tourist for the year {year}: {predicted_income}")
